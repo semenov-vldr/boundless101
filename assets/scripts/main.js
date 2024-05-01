8476d461368672c2d17abd1d44c5eb34b466ac5c
semenov-vldr <semenov-vldr@users.noreply.github.com>
2024-05-01 07:53:31 +0000

    deploy: 180d8109159ed2fa7b9ca78f41c62cf94b932377

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -233,6 +233,31 @@ mobileNav();
 "use strict";
 "use strict";
 
+var swiperInst = document.querySelector('.institutions__list.swiper');
+var mySwiper = new Swiper(swiperInst, {
+  breakpoints: {
+    1100: {
+      slidesPerView: 2.4,
+      spaceBetween: 30
+    },
+    940: {
+      slidesPerView: 2.5
+    },
+    640: {
+      slidesPerView: 2.3,
+      spaceBetween: 24
+    },
+    480: {
+      slidesPerView: 1.8
+    },
+    320: {
+      slidesPerView: 1.2,
+      spaceBetween: 12
+    }
+  }
+});
+"use strict";
+
 function hideLoader() {
   var loader = document.getElementById('loader');
   if (loader) {

481a936861715aeef4f4812ad200835e4bcf3565
Vladimir <vova2066@yandex.ru>
2024-05-22 01:14:05 +0300

    add sa-francisco, miami

--- src/blocks/detail-course/detail-course.js
+++ src/blocks/detail-course/detail-course.js
@@ -1,18 +1,15 @@
-const detailCourse = document.querySelector(".detail-course.detail-course--tabs");
+const detailCourseTabs = document.querySelectorAll(".detail-course__tab[data-tab]");
 
-if (detailCourse) {
+if (detailCourseTabs) {
 
-  const tabs = detailCourse.querySelectorAll(".detail-course__tab[data-tab]");
-  const bodyList = detailCourse.querySelectorAll(".detail-course__body[data-tab]");
+  const bodyList = document.querySelectorAll(".detail-course__body[data-tab]");
 
-  tabs.forEach(tab => {
+  detailCourseTabs.forEach(tab => {
     const dataTab = tab.dataset.tab;
 
     tab.addEventListener("click", () => {
-
-      tabs.forEach(tab => tab.classList.remove("js-active"));
+      detailCourseTabs.forEach(tab => tab.classList.remove("js-active"));
       tab.classList.add("js-active");
-
       bodyList.forEach(bodyItem => bodyItem.classList.add("js-hidden"));
 
       bodyList.forEach(bodyItem => {

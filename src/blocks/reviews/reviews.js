8fc5fe5b05cf95cc77e467a17312e3cbd8b8f691
Vladimir <vova2066@yandex.ru>
2024-08-25 09:58:29 +0300

    add reviews

--- src/blocks/reviews/reviews.js
+++ src/blocks/reviews/reviews.js
@@ -1,8 +1,29 @@
-var swiperReviews = new Swiper(".reviews__swiper", {
-  direction: "vertical",
-  slidesPerView: "auto",
-  freeMode: true,
-  mousewheel: true,
-  //centeredSlides: true,
-  //initialSlide: 1,
+const swiperReviews = new Swiper(".reviews__swiper", {
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    types: "bullets",
+  },
+
+  breakpoints: {
+    320: {
+      direction: "horizontal",
+      slidesPerView: 1.2,
+      spaceBetween: 40,
+    },
+    640: {
+      direction: "horizontal",
+      slidesPerView: 2.2,
+      spaceBetween: 40,
+    },
+    1000: {
+      direction: "vertical",
+      slidesPerView: "auto",
+      freeMode: true,
+      mousewheel: true,
+      spaceBetween: 32,
+      centeredSlides: true,
+    }
+  }
 });
